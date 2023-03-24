daf3c066d1acbb6d579020c1b07660577d27e080
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2023-03-24 11:23:42 +0100

    feat: add nav animation with framer motion

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,6 +1,7 @@
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 
-import { clsxm } from '@/lib/clsxm';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
 
 const links = [
   { href: '/', label: 'Products' },
@@ -12,19 +13,29 @@ interface NavigationProps {
   isNavVisible: boolean;
 }
 
-export const Navigation = ({ isNavVisible }: NavigationProps) => (
-  <nav className='flex items-center'>
-    <ul
-      className={clsxm(
-        isNavVisible ? 'translate-x-0' : 'translate-x-full',
-        'fixed inset-0 bg-white flex flex-col items-center justify-center gap-8 md:static md:inset-auto md:bg-transparent md:flex-row md:justify-start md:translate-x-0 font-medium text-lg'
-      )}
-    >
-      {links.map(({ href, label }) => (
-        <li key={label}>
-          <Link href={href}>{label}</Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '-100%' },
+};
+
+export const Navigation = ({ isNavVisible }: NavigationProps) => {
+  const matches = useMediaQuery('(min-width: 768px)');
+
+  return (
+    <nav className='flex items-center'>
+      <motion.ul
+        initial={matches ? variants['open'] : variants['closed']}
+        animate={isNavVisible ? 'open' : 'closed'}
+        variants={!matches ? variants : {}}
+        transition={{ type: 'tween' }}
+        className='fixed inset-0 -translate-x-full bg-white flex flex-col items-center justify-center gap-8 md:static md:inset-auto md:bg-transparent md:flex-row md:justify-start font-medium text-lg'
+      >
+        {links.map(({ href, label }) => (
+          <li key={label}>
+            <Link href={href}>{label}</Link>
+          </li>
+        ))}
+      </motion.ul>
+    </nav>
+  );
+};
