6d5fb443a1feadddea5d44760fa812a0622bbe81
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2023-03-31 19:10:48 +0200

    feat: add favourites section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Approach } from '@/components/Approach';
 import { Arrivals } from '@/components/Arrivals';
+import { Favourites } from '@/components/Favourites';
 import { Hero } from '@/components/Hero';
 import { Mission } from '@/components/Mission';
 import { Newsletter } from '@/components/Newsletter';
@@ -9,6 +10,7 @@ const Home = () => (
     <Hero />
     <Arrivals />
     <Mission />
+    <Favourites />
     <Approach />
     <Newsletter />
   </>
